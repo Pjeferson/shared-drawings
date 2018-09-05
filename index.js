e348a7bbb2368c7a4a1304d3a2dbd0770af33c73
Pjeferson <paulojeferson34@gmail.com>
2018-09-05 16:45:23 -0300

    sending draws in broadcast

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+let io;
 
 app.use(express.static(__dirname+'/public'));
 
 const server = app.listen(3000, () =>{console.log('listening at 3000')});
 
+io = socket(server);
+
+io.sockets.on('connection', (socket) => {
+    console.log(`new connection with ${socket.id}`);
+
+    socket.on('drawn', (data) => {
+        socket.broadcast.emit('drawn', data);
+    })
+});
