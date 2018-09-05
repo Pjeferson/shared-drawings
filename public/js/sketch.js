c87d9a6175fc14d3505491e3d6120053f284d0e1
Pjeferson <paulojeferson34@gmail.com>
2018-09-05 16:44:35 -0300

    connecting with server

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,11 +1,30 @@
-
+let socket; 
+let url;
 
 function setup() {
   createCanvas(640,480);
- 
+  background(51);
+
+  url = 'http://localhost:3000/';
+  //url = 'http://192.168.0.27:3000/'; //an exemple for local network connection
+  socket = io.connect(url);
+
+  socket.on('drawn', (data) => {
+    newDrawn(data.x, data.y);
+  });
 }
 
-function draw() {
-  background(51);
+function mouseDragged(){
+  newDrawn(mouseX,mouseY);
+  socket.emit('drawn', {x: mouseX, y: mouseY});
+}
+
+function newDrawn(x,y){
+  noStroke();
+  fill(255);
+  ellipse(x, y, 20,20);
+}
+
+function draw() {// Unused
   
 }
